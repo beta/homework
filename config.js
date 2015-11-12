93d1e17fe9100b4a91ba04e13363eb3de0e35831
Beta Kuang <beta.kuang@gmail.com>
2015-11-12 16:47:53 +0800

    Fixed config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var _config = {
   
   // GitHub configuations.
-  username: '',
-  repo: '',
+  username: 'beta',
+  repo: 'homework',
   access_token: 'de2180d08cb69bc5da4' + 'dee7a23b4834fc4a94dcd',
   
   // Validation.
