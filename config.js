10adac076e94891811250cee59f5ce928f760d57
Beta Kuang <beta.kuang@gmail.com>
2016-04-12 10:24:22 +0800

    Roll back to original repo

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var _config = {
   
   // GitHub configuations.
   username: 'beta',
-  repo: 'homework-old',
+  repo: 'homework',
   access_token: 'de2180d08cb69bc5da4' + 'dee7a23b4834fc4a94dcd',
   
   // Validation.
